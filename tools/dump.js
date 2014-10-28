d6a93455beff709fb02b27393d7e2d2263c24275
Lucio Paiva <luciopaiva@gmail.com>
2014-10-27 22:14:21 -0200

    Object dump tool

---
+++ tools/dump.js
@@ -0,0 +1,53 @@
+"use strict";
+
+var
+    builder = require('../lib/docbuilder');
+
+var
+    doc;
+
+
+function printObjects() {
+    var
+        objs, keys;
+
+    objs = doc.getAllObjects();
+    keys = Object.keys(objs);
+
+    keys.forEach(function (key) {
+
+        console.info('\n# %s ###########################################################################', key);
+        console.dir(objs[key]);
+    });
+}
+
+function dump(filename) {
+
+    try {
+        doc = builder(filename);
+
+        printObjects();
+
+    } catch (e) {
+        if (e.code === 'ENOENT') {
+            console.error('File "%s" was not found or could not be opened.', filename);
+        } else {
+            console.error(e);
+        }
+    }
+}
+
+function main() {
+
+    if (process.argv.length !== 3) {
+
+        console.error('How to use:');
+        console.error('\tnode dump <pdf_file>');
+
+    } else {
+
+        dump(process.argv[2]);
+    }
+}
+
+main();
